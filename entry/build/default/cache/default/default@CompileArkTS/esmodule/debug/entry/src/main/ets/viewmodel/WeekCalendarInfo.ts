bb2703e4889dd3c1c9657c33e059fad7bbac7e95
ZephyrVictor <empire258700@gmail.com>
2024-07-17 10:05:38 +0800

    清理了不必要的文件

--- entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/viewmodel/WeekCalendarInfo.ts
+++
@@ -1,9 +0,0 @@
-import type { WeekDateModel } from '../model/WeekCalendarModel';
-/**
- * Week calendar info
- *
- */
-export default class WeekCalendarInfo {
-    arr: Array<WeekDateModel> = [];
-    strArr: Array<string> = [];
-}
